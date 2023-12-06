c91560cd09fdfcb75d4ecad176f255ee948a5736
noah <noah.tonda@condorcet.be>
2023-12-06 14:26:17 +0100

    ajout code SuperTieBreak

--- Models/SuperTieBreak.cs
+++ Models/SuperTieBreak.cs
@@ -1,13 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjetTennis.Models
 {
-    internal class SuperTieBreak
+    internal class SuperTieBreak : Sets
     {
-        public Sets Sets { get; set; }
+        public void Play()
+        {
+            Random random = new Random();
+            int PointOp1 = 0, PointOp2 = 0;
+            int hit = random.Next(0, 1);
+            int winningPlayer = random.Next(0, 2);
+
+
+            do
+            {
+
+
+
+                winningPlayer = random.Next(0, 2);
+
+                if (winningPlayer == 0)
+                {
+                    PointOp1++;
+                }
+                else
+                {
+                    PointOp2++;
+                }
+
+            } while (!(PointOp1 >= 10 || PointOp2 >= 10) || (Math.Abs(PointOp1 - PointOp2) < 2));
+
+
+        }
+
+
+
     }
 }
